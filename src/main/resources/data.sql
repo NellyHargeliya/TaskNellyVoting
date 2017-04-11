9a709302311a28cfa72d811ce46dbd4ccc28fe80
Nelly Hargeliya <nelly.hargeliya@gmail.com>
2017-04-11 22:21:54 +0300

    Delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO theme(nameTheme,startVoting, endVoting,urlTheme) values( 'iutamkk','2017-01-01','2017-01-02','tyt.by');
-INSERT INTO theme(nameTheme,startVoting, endVoting,urlTheme) values( 'jjjj','2017-01-01','2017-01-02','tyt.by');
-INSERT INTO theme(nameTheme,startVoting, endVoting,urlTheme) values( 'oooo','2017-01-01','2017-01-02','tyt.by');
