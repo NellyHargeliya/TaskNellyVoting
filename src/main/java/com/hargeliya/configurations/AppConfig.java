b7bdefd4a03c4201f44de8af183936b1847fbf02
NellyHargeliya <nelly.hargeliy@gmail.com>
2017-04-27 23:53:16 +0300

    return confic jdbc

--- src/main/java/com/hargeliya/configurations/AppConfig.java
+++ src/main/java/com/hargeliya/configurations/AppConfig.java
@@ -1,16 +1,10 @@
 package com.hargeliya.configurations;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 public class AppConfig extends WebMvcConfigurerAdapter {
